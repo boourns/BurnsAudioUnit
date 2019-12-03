6345cf79e9b337c945d57c7ccfe6b55e8b1df2e2
Tom Burns <tom@burns.ca>
2019-12-03 12:29:36 -0500

    import

---
+++ BurnsAudioUnit/BurnsAudioUnit.h
@@ -0,0 +1,19 @@
+//
+//  BurnsAudio.h
+//  BurnsAudio
+//
+//  Created by tom on 2019-11-23.
+//  Copyright © 2019 tom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BurnsAudio.
+FOUNDATION_EXPORT double BurnsAudioUnitVersionNumber;
+
+//! Project version string for BurnsAudio.
+FOUNDATION_EXPORT const unsigned char BurnsAudioUnitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BurnsAudioUnit/PublicHeader.h>
+
+#import <BurnsAudioUnit/MIDIProcessorWrapper.h>
